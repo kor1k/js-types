b62606a654340c2e4d9f40c81da04129bb8b20c7
Kostya <koortttt@gmail.com>
2019-01-11 19:53:08 +0200

    практика по типам, алертам и преобразованию

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// let a =123;
+//
+// let b = false;
+//
+// let c = [];
+//
+// console.log(a + "");  //cast in String from Num
+// console.log(b + '');  //cast in boolean from Num                OR // console.log(String(a));
+// console.log(c + '');  // cast in 'hui znaet vo chto' from Array
+
+
+// let b = true;
+// let c = false;
+// let a = [];
+// let a1 = [1,2,2,334];
+// console.log(+b);
+// console.log(b);
+// console.log(+c);
+// console.log(c);
+// console.log(+a);
+// console.log(a);
+// console.log(+a1);
+// console.log(a1);
+
+
+// let a = '111.11aaaa22.22';
+// console.log(+a);
+// console.log(parseInt(a)); // выбирает только цыфру, когда он видет стрингу, то он её не хавает
+// console.log(parseFloat(a)); // выбирает только цыфру, когда он видет стрингу, то он её не хавает
+//
+// console.log((12,1 * 3) * 2); // =6, spasibo JS. (ЦЫФРА ДО ЗАПЯТОЙ ИГНОРИРУЮТЬСЯ)
+// console.log(2**5); // возведение в степень
+
+// let deadline = +prompt('enter deadline in days');
+// if (isNaN(deadline)){
+//     alert(`u r studip`)
+// } else {
+//     alert(`deadline is ${deadline+11}`)
+// }
+
+
+
+let number = prompt('enter ur number');
+
+console.log(`Full number is: ${'+38'+number}`);
+
